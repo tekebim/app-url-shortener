579cfbaefac06ada3e7570d320cfc789d1dd037c
Michael R <tekmedias@gmail.com>
2022-01-24 10:56:12 +0100

    feat(app): use body parser to allow json data on body post

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import config from 'config';
 import routes from "./routes";
 import database from "./database";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = config.get('port');
 
+app.use(bodyParser.json());
+
 app.listen(port, () => {
     console.log(`Application is listening on ${port}`);
-    database().then(r => {
-        console.log(r)
-    });
+    database();
     routes(app);
 })
